ca40c52e50d15fe1747b13796ac2c177866c9422
Rohanpramanik <pramanikrohan336@gmail.com>
2025-09-06 15:54:47 +0530

    added server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,14 +6,23 @@ app.listen(5000, ()=>{
     console.log("server started on port 5000");
 })
 
-app.get("/",(req, res)=>{
+app.get("/abc",(req, res)=>{
     res.send("hello hello")
 });
 
-app.use("/test", (req, res)=>{
+app.get("/test/:roh", (req, res)=>{
+    console.log(req.params);
     res.send("hello from test")
 })
 
-app.use("/check", (req, res)=>{
-    res.send("hello from check")
+// http://localhost:5000/test?userId=78&password=itsme
+app.get("/test", (req, res)=>{
+    console.log(req.query);
+    // { userId: '78', password: 'itsme' }
+    res.send("hello from test")
+})  
+
+// matches for all if it start with ab and end with de
+app.get("/ab*de", (req, res)=>{
+    res.send({name:"rohan",age:25})
 })
