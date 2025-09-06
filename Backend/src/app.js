66b2df0ea948729e14adb255a180bb1837354fed
Rohanpramanik <pramanikrohan336@gmail.com>
2025-09-06 16:29:20 +0530

    multiple route handler

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,27 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use("/user",(req,res,next)=>{
+        console.log("Router handler 1");
+        // res.send("Route handler 1")
+        next();
+    },(req,res)=>{
+        console.log("Router handler 2")
+        res.send("Route handler 2")
+    }
+);
+
 app.listen(5000, ()=>{
     console.log("server started on port 5000");
-})
-
-app.get("/abc",(req, res)=>{
-    res.send("hello hello")
-});
-
-app.get("/test/:roh", (req, res)=>{
-    console.log(req.params);
-    res.send("hello from test")
-})
-
-// http://localhost:5000/test?userId=78&password=itsme
-app.get("/test", (req, res)=>{
-    console.log(req.query);
-    // { userId: '78', password: 'itsme' }
-    res.send("hello from test")
-})  
-
-// matches for all if it start with ab and end with de
-app.get("/ab*de", (req, res)=>{
-    res.send({name:"rohan",age:25})
 })
